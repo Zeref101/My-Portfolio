85c3002d87c0424a38832740e6461613d9edb15e
Shreyas <shreyas.mohanty03@gmail.com>
2023-03-28 11:11:45 +0530

    adding seperate js file

---
+++ script.js
@@ -0,0 +1,38 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents")
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemeu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemeu.style.right = "0";
+}
+function closemenu() {
+    sidemeu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzs21eoZGN_qn2XBkfMpEkp33KsS-61owI51H9BOITzU4QfeKELUnq2tmCjPV_kFZ1B/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
